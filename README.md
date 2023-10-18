354791bc9f063f67973916b8263dd3c3faa59adf
Vittorio Ricciardulli <61703242+vittorio-dealroom@users.noreply.github.com>
2023-10-18 20:40:00 +0200

    DRP-4203: Create Cloud Run auth python package (#1)
    * Update .gitignore
    
    * Delete main.py
    
    * Create export_requirements.sh
    
    * Create package definition
    
    * Dependencies
    
    * Update pull_request_template.md
    
    * Package source code
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update __init__.py
    
    * Update README.md

--- README.md
+++ README.md
@@ -1,15 +1,145 @@
-# (UPDATE THIS README AFTER USING THE TEMPLATE)
------
-# data-minimal-python-template
-A minimal python-repository template for the data team. Any new repository should be created at least from this template.
+# Cloud Run Auth
 
-## How to use this template
-- [ ] Use this template
-- [ ] Update `name` & `description` in [pyproject.toml](pyproject.toml)
-- [ ] Update this [README.md](README.md)
+Make authorized requests to private services in GCP using a `requests`-like interface.
 
+The caller may be an authorized user (e.g. a developer using this library locally), or an authorized Service Account (e.g. a DataFlow job that needs to call one or more services).
+
+Terminology:
+
+* **Service**: a Cloud Run or a Cloud Function.
+
+* **Authorized**: a caller that has the appropriate role to call the service: `roles/run.invoker` for Cloud Run and `roles/cloudfunctions.invoker` for Cloud Functions.
+
+## Install
+
+* **Poetry**:
+
+    ```shell
+    poetry add git+https://github.com/dealroom/data-cloud-run-auth.git#main
+    ```
+
+* **pip**:
+
+    ```shell
+    pip install git+https://github.com/dealroom/data-cloud-run-auth.git@main
+    ```
+
+## Usage
+
+This package provides a session-like object that can be used to make authorized requests to a Cloud Run or a Cloud Function.
+
+In 90% of cases, you'd use it like so:
+
+* make authorized requests to a Cloud Run:
+
+    ```python
+    from dealroom_cloud_run_auth import create_session
+
+    cloud_run_url = "https://my-cloud-run-service.com"
+    session = create_session(cloud_run_url)
+    # Using "/my-endpoint" or "my-endpoint" makes no difference
+    response = session.get("/my-endpoint")
+    ```
+
+* make authorized requests to a Cloud Function:
+
+    ```python
+    from dealroom_cloud_run_auth import create_session
+
+    # Use the full URL for Cloud Functions, as the base URL is the same for
+    # all functions in a certain zone
+    cloud_function_url = "https://cloud-functions.com/function-name"
+    session = create_session(cloud_function_url)
+    response = session.post("", json={"key": "value"})
+    ```
+
+For the remaining 10%:
+
+* you may want to set a custom timeout:
+
+    ```python
+    ...
+    session = create_session(cloud_run_url, timeout=10)
+    ...
+    ```
+
+* you may want to set a custom user agent. For example, the user agent is not set when you use this package locally, so you could set it yourself:
+
+    ```python
+    ...
+    session = create_session(cloud_run_url, user_agent="my-user-agent")
+    ...
+    ```
+
+⚠️ You should **NOT** need to set the user agent in production, as it is set automatically to a value that identifies the caller.
+
+To know more about these and other parameters, check out the docstrings of class `AuthorizedBaseUrlSession`.
+
+## Best practices
+
+* Organize each service in its own module and implement each endpoint as a function that internally uses the session to make the request. Then keep all these modules in a parent module `services/`. For example, your project could look like this:
+
+    ```text
+    app/
+    ├── services/
+    │   ├── __init__.py
+    │   ├── service_1.py
+    │   └── service_2/
+    │       ├── __init__.py
+    │       ├── movies.py
+    │       └── games.py
+    └── main.py
+    ```
+
+    where `service_2/` would be a service that has a more complicated logic and/or many endpoints, while `service_1.py` would be a simpler service. The contents of this latter could be:
+
+    ```python
+    """
+    Service 1 has two endpoints: "/1" and "/2". They are both
+    GET endpoints. "/1" returns a string. "/2" returns a
+    JSON object.
+
+    We implement each call to these endpoints as functions that return the data
+    we get. Inside these functions we also handle exceptions, bad status codes,
+    bad data, etc.
+    """
+    from dealroom_cloud_run_auth import create_session
+
+    _session = create_session("https://my-cloud-run-service.com")
+
+
+    def get_1() -> str | None:
+        # Handle errors and so on...
+        return _session.get("/1").text
+
+
+    def get_2() -> dict:
+        # Handle errors and so on...
+        return _session.get("2").json()
+    ```
+
+## Troubleshooting
+
+If for any reason the library cannot get the ID token credentials to call the service, it will raise a `google.auth.exceptions.DefaultCredentialsError` exception.
 
 ## Development
-- `poetry install`
-- `poetry shell`
-- `python main.py`
+
+* Install environment and activate it:
+
+    ```shell
+    poetry install && poetry shell
+    ```
+
+* Refresh requirements files:
+
+    ```shell
+    ./scripts/export_requirements.sh
+    ```
+
+## References
+
+* This package is based on the [Airflow module to get ID token credentials in GCP](https://github.com/apache/airflow/blob/b1196460db1a21b2c6c3ef2e841fc6d0c22afe97/airflow/providers/google/common/utils/id_token_credentials.py#L1)
+
+* [Google Cloud Run - Authenticating service-to-service](https://cloud.google.com/run/docs/authenticating/service-to-service)
+
+* [Google Cloud Run - Authenticating developers](https://cloud.google.com/run/docs/authenticating/developers)
---
+++ dealroom_cloud_run_auth/session.py
@@ -0,0 +1,226 @@
+import os
+from typing import Any, Callable
+from urllib.parse import urljoin
+
+from google.auth.transport import Response
+from google.auth.transport.requests import AuthorizedSession as DefaultAuthedSession
+
+from dealroom_cloud_run_auth.id_token_credentials import (
+    get_default_id_token_credentials,
+)
+
+URLJoiner = Callable[[str, str], str]
+Timeout = float | tuple[float, float]
+
+_DEFAULT_TIMEOUT = 120.0
+
+
+def default_url_joiner(base_url: str, path: str) -> str:
+    """Use `urllib.parse.urljoin` to join the base URL and the path."""
+    return urljoin(base_url + "/", path)
+
+
+def default_user_agent() -> str | None:
+    k_service = os.getenv("K_SERVICE")
+    k_revision = os.getenv("K_REVISION")
+    if k_service and k_revision:
+        return f"{k_service}/{k_revision}"
+    # These cases are unlikely to happen, but just in case
+    elif k_service:
+        return k_service
+    elif k_revision:
+        return k_revision
+
+
+class AuthorizedBaseUrlSession(DefaultAuthedSession):
+
+    """
+    Session-like object that authorizes service-to-service requests through an
+    ID token. The ID token is authorized for the target audience, which is the
+    base URL used to initialize this session.
+
+    It is a subclass of google.auth.transport.requests.AuthorizedSession that
+    adds a number of features that come in handy for the services architecture
+    used by the Data Team at Dealroom:
+
+        * a URL that all requests will use as base:
+
+        ```
+        >>> cloud_run_url = "https://my-cloud-run-url.com"
+        >>> session = AuthorizedBaseUrlSession(cloud_run_url)
+        >>> response = session.get("resource")
+        >>> print(response.request.url)
+        https://my-cloud-run-url.com/resource
+        ```
+
+        By default, the base URL and the path are combined using `urljoin`. This
+        works fine for most of the cases, but if it doesn't, you can override this
+        behavior by passing a suitable callable to the `url_joiner` parameter:
+
+        ```
+        >>> cloud_run_url = "https://my-cloud-run-url.com"
+        >>> session = AuthorizedBaseUrlSession(
+        ...     cloud_run_url,
+        ...     url_joiner=lambda base_url, path: base_url + "/" + path
+        ... )
+        >>> response = session.get("resource")
+        >>> print(response.request.url)
+        https://my-cloud-run-url.com/resource
+        ```
+
+        More info: https://github.com/requests/toolbelt/blob/master/requests_toolbelt/sessions.py
+
+        * the `base_url` is used to initialize the credentials used to call that
+        same service:
+
+        ```
+        >>> cloud_run_url = "https://my-cloud-run-url.com"
+        >>> session = AuthorizedBaseUrlSession(cloud_run_url)
+        >>> # Make a request to the Cloud Run, so that the token is generated
+        >>> response = session.get("resource")
+        >>> print(response.request.headers["Authorization"])
+        # Prints a JWT token that authorizes requests to that Cloud Run.
+        # Lasts for 1 hour
+        ```
+
+        The underlying credentials object takes care of refreshing the token
+        when it's expired.
+
+        More info: https://cloud.google.com/run/docs/authenticating/service-to-service
+
+        * can set header "User-Agent" from the constructor:
+
+        ```
+        >>> cloud_run_url = "https://my-cloud-run-url.com"
+        >>> user_agent = "my-user-agent"
+        >>> session = AuthorizedBaseUrlSession(cloud_run_url, user_agent=user_agent)
+        >>> print(session.headers["User-Agent"])
+        my-user-agent
+        ```
+
+        It defaults to a suitable combination of env vars `K_SERVICE` and
+        `K_REVISION`. These are reserved env vars in Cloud Run and Cloud Functions
+        that are set to the name of the service. It is recommended to use this
+        default, or provide a non-null value, as the user-agent appears in the
+        request logs of the receiving service and thus can be used for debugging
+        and monitoring.
+
+        When None, it defaults to the User-Agent from the requests library:
+        https://github.com/psf/requests/blob/2c2138e811487b13020eb331482fb991fd399d4e/requests/utils.py#L808
+
+        More info:
+        https://cloud.google.com/run/docs/container-contract#services-env-vars
+        https://cloud.google.com/functions/docs/configuring/env-var#setting_build_environment_variables
+        https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/User-Agent
+
+        * can set a session-wide timeout:
+
+        ```
+        >>> cloud_run_url = "https://my-cloud-run-url.com"
+        >>> timeout = 5
+        >>> session = AuthorizedBaseUrlSession(cloud_run_url, timeout=timeout)
+        >>> print(session.timeout)
+        5
+        ```
+
+        Any request-specific timeout will override the session-wide timeout for
+        that specific request only.
+
+        Notice that the timeout is already set to 120 seconds by default.
+
+        * can turn off keep-alive:
+
+        ```
+        >>> cloud_run_url = "https://my-cloud-run-url.com"
+        >>> session = AuthorizedBaseUrlSession(cloud_run_url, keep_alive=False)
+        >>> print(session.headers["Connection"])
+        close
+        ```
+
+        This forces the connection to close after each request. In general, it's
+        recommended to leave this as True, as it improves performance by reusing
+        the connection.
+
+        More info: https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Connection
+    """
+
+    def __init__(
+        self,
+        base_url: str,
+        *,
+        user_agent: str | None = None,
+        timeout: Timeout = _DEFAULT_TIMEOUT,
+        url_joiner: URLJoiner = default_url_joiner,
+        keep_alive: bool = True,
+    ) -> None:
+        credentials = get_default_id_token_credentials(target_audience=base_url)
+        super().__init__(credentials)
+
+        self._base_url = base_url.rstrip("/")
+        self._url_joiner = url_joiner
+        self._timeout = timeout
+
+        if ua := (user_agent or default_user_agent()):
+            self.headers["User-Agent"] = ua
+
+        if not keep_alive:
+            self.headers["Connection"] = "close"
+
+    @property
+    def base_url(self) -> str:
+        return self._base_url
+
+    @property
+    def user_agent(self) -> str | bytes:
+        return self.headers["User-Agent"]
+
+    @property
+    def timeout(self) -> Timeout:
+        return self._timeout
+
+    @property
+    def url_joiner(self) -> URLJoiner:
+        return self._url_joiner
+
+    def request(self, method: str, path: str, *args, **kwargs) -> Any | Response:
+        if kwargs.get("timeout") is None and self._timeout:
+            # Not sure this is ok
+            kwargs["timeout"] = self._timeout
+
+        url = self._url_joiner(self._base_url, path)
+        return super(AuthorizedBaseUrlSession, self).request(
+            method, url, *args, **kwargs
+        )
+
+
+def create_session(
+    base_url: str,
+    *,
+    user_agent: str | None = None,
+    timeout: Timeout = _DEFAULT_TIMEOUT,
+    url_joiner: URLJoiner = default_url_joiner,
+    keep_alive: bool = True,
+) -> AuthorizedBaseUrlSession:
+    """Returns a session object that can make authorized requests to a service.
+
+    Args:
+        base_url: The base URL of the service to authorize requests to. This is
+            also used as the target audience to generate the ID token.
+        url_joiner: A callable that takes a base URL and a path and returns the
+            full URL. Defaults to `urllib.parse.urljoin`.
+        user_agent: The value to set in the "User-Agent" header. Defaults to a
+            suitable combination of env vars `K_SERVICE` and `K_REVISION`. It is
+            recommended to keep this default in production environments.
+        timeout: A session-wide timeout. Defaults to 120 seconds. Request-specific
+            timeouts override this value for that specific request only.
+        keep_alive: Whether to reuse the connection after each request. Defaults
+            to `True`. It is recommended to leave this as is, as it improves
+            performance by reusing the connection.
+    """
+    return AuthorizedBaseUrlSession(
+        base_url=base_url,
+        url_joiner=url_joiner,
+        user_agent=user_agent,
+        timeout=timeout,
+        keep_alive=keep_alive,
+    )
