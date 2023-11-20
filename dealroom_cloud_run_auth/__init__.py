ff7360d8de7d2b646c9ebc8358625963aac5df89
Vittorio Ricciardulli <61703242+vittorio-dealroom@users.noreply.github.com>
2023-11-20 09:48:58 +0100

    Expose `AuthorizedBaseUrlSession` class (#2)

--- dealroom_cloud_run_auth/__init__.py
+++ dealroom_cloud_run_auth/__init__.py
@@ -8,4 +8,4 @@ To decode and obtain info about this token, run the following commands:
     ID_TOKEN="$(python -m dealroom_cloud_run_auth)"
     curl "https://www.googleapis.com/oauth2/v3/tokeninfo?id_token=${ID_TOKEN}"
 """
-from dealroom_cloud_run_auth.session import create_session
+from dealroom_cloud_run_auth.session import AuthorizedBaseUrlSession, create_session
